50c74a5101bc89577ae5a34db3ff135120d90e6f
Shubham Pathak <spathak.2102@gmail.com>
2026-01-06 13:02:28 +0100

    contour surfaces visualize pyvista

--- pyvista_gempy_local_anisotropy.py
+++ pyvista_gempy_local_anisotropy.py
@@ -1042,26 +1042,23 @@ def main():
 
 
     ##### FOR 3D matplotlib #####
-    import time
-    for t in [-0.5, 0, 0.5, .75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5, 3, 3.5, 4,4.5]:
-        gp.plot_data_section(section={4:t}, plot_scalar_field = True, plot_input_data=True)
-        time.sleep(1)
+    # import time
+    # for t in [-0.5, 0, 0.5, .75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5, 3, 3.5, 4,4.5]:
+    #     gp.plot_data_section(section={4:t}, plot_scalar_field = True, plot_input_data=True)
+    #     time.sleep(1)
 
 
     #############################################################################################
     ########## Uncomment below for  Interactive Visualization Pyvista below  ####################
     #############################################################################################
     
-    
-    
-
     ###############################################################
     ########### show/unshow input data using "plot_input_data" argument
     ########### show/unshow surface or interfaces using "only_surface_mode" argument
     ###############################################################
 
-    # print("\nStarting Interactive Visualization...")
-    # gp.plot_interactive_section(plot_input_data = True, only_surface_mode = False)
+    print("\nStarting Interactive Visualization...")
+    gp.plot_interactive_section(plot_input_data = True, only_surface_mode = False)
 
 
 if __name__ == "__main__":
